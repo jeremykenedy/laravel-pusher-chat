69057b17fa5c8ba182146c6fc51a27740ee08ef9
Jeremy <jeremykenedy@gmail.com>
2020-05-24 05:41:52 -0700

    init

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Chat;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('chat.chat');
+    }
+
+    /**
+     * Fetch all chat messages from the database.
+     *
+     * @return array \App\Chat
+     */
+    public function fetchAllMessages()
+    {
+        return Chat::with('user')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function sendMessage(Request $request)
+    {
+        $chat = auth()->user()->messages()->create([
+            'message' => $request->message
+        ]);
+
+        broadcast(new ChatEvent($chat->load('user')))->toOthers();
+
+        return ['status' => 'success'];
+    }
+}
